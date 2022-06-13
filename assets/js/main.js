f53ede2d430b7f67a883cfe4e19293ba071c1746
GustavoEmanuel1995 <gustavo38893730@gmail.com>
2022-06-13 20:44:26 -0300

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+function guardarTarea(){
+    var tareaInput = document.getElementById("input-tarea")
+    var listado = document.getElementById("Listado-tareas")
+
+    listado.innerHTML += `<li>${tareaInput.value}</li>`
+    tareaInputvalue = "" 
+}
