dd5efc80d67f7d41d724f97f9047af4872602f11
GustavoEmanuel1995 <gustavo38893730@gmail.com>
2022-06-15 18:18:43 -0300

    fix listado
    fix listado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,11 @@
 function guardarTarea(){
+
     var tareaInput = document.getElementById("input-tarea")
-    var listado = document.getElementById("Listado-tareas")
+
+    var listado = document.getElementById("listado-tareas")
 
     listado.innerHTML += `<li>${tareaInput.value}</li>`
+
     tareaInput.value = "" 
+    
 }
