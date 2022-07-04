e9f639f8a7157600f000d16cce94054e303df164
GustavoEmanuel1995 <gustavo38893730@gmail.com>
2022-07-04 18:43:18 -0300

    Agrego rama dev
    Agrego rama dev

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,50 +1,51 @@
- // function guardarTarea(){
-
-//     var tareaInput = document.getElementById("input-tarea")
-
-//     var listado = document.getElementById("listado-tareas")
-
-//     listado.innerHTML += `<li>${tareaInput.value}</li>`
-
-//     tareaInput.value = ""
-
-// }//ctrl + k + c
-
+// }//ctrl + k + c, {}, [], @, &, #, ``, '',  $
 var listadoTareas = [];
+leerTareasLocalStorage()
+listadoTareas.map(t => addTareaHtml(t))
 
-document.addEventListener("submit", function(event){
-
+document.addEventListener("submit", function(event){ 
     event.preventDefault();
     var tarea = event.target[0].value
+    if(tarea == ""){ //Comprobamos si esta vacio o no 
+        return alert("Por favor completar el campo tarea") // return Evita que se guarde algo vacio
+    }
+    escribirTareaLocalStorage(tarea)
+    event.target[0].value = "" //Limpia en input
+})
 
-    if(tarea == ""){
-        return alert("Por favor completar el campo tarea")
+function addTareaHtml(tarea){ //agrega el listado de tareas al HTML.
+    var listadoHTML = document.getElementById("listado-tareas")
+    listadoHTML.innerHTML += `<li class="list-group-item">${tarea}</li>`
+}
+
+function leerTareasLocalStorage(){
+    var listadoTareasLS = JSON.parse(localStorage.getItem('tareas'))
+
+    if (listadoTareasLS?.length > 0) {
+        this.listadoTareas = listadoTareasLS
+        console.log(`Se cargaron ${listadoTareasLS.length} tareas desde el localStorage`)
+        $.notify(`se cargaron ${listadoTareasLS.length} tareas desde el localStorage`,"success");
+    } else {
+        $.notify(`no se encontraron tares en el localStorage`,"info");
+        console.log("No se encontraro tareas en el localStorage")
     }
 
-    listadoTareas.push(tarea)
 
-    console.log(listadoTareas)
 
-    event.target[0].value = ""
-    addTareaHtml()
-})
-function addTareaHtml(){
-    // for (let index = 0; index < listadoTareas.length; index++) {
-    //     console.log(listadoTareas[index]);
-        
-    // }
-
-    // listadoTareas.forEach(function(e){
-    //     console.log(e)
-    // })
-
-    var listadoHTML = document.getElementById("listado-tareas")
-
-    // listadoTareas.map(function(t){
-    //     listadoHTML.innerHTML += `<li>${t}</li>`
-    // })
-    
-    const ultimaTarea = listadoTareas[listadoTareas.length - 1]
-    listadoHTML.innerHTML += `<li>${ultimaTarea}</li>`
-    
+}
+function escribirTareaLocalStorage(tarea) {
+    var listadoTareasLS = JSON.parse(localStorage.getItem('tareas'))
+    if (!listadoTareasLS) {
+        localStorage.setItem('tareas',JSON.stringify([tarea]))
+        leerTareasLocalStorage()
+        addTareaHtml(tarea)
+        $.notify(`Tarea cargada`,"success");
+        return console.log("Tarea cargada")
+    }
+    listadoTareasLS.push(tarea)
+    localStorage.setItem('tareas',JSON.stringify(listadoTareasLS))
+    leerTareasLocalStorage()
+    addTareaHtml(tarea)
+    $.notify(`Tarea cargada`,"success");
+    console.log("Tarea cargada")
 }
